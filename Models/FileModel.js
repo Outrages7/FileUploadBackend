b7307032ce346f71844743616ff24a0e3ad7809a
Shubham <rana.772003shubham@gmail.com>
2025-12-26 19:22:38 +0530

    added mailing service

--- Models/FileModel.js
+++ Models/FileModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 const fileSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -13,6 +14,36 @@ const fileSchema = new mongoose.Schema({
     email:{
         type:String,
     }
+});
+
+// Post Middleware
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log(doc);
+
+    // 1️⃣ Create transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST, // smtp.gmail.com
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // 2️⃣ Send mail
+    let info = await transporter.sendMail({
+      from: `"File Upload App" <${process.env.MAIL_USER}>`,
+      to: doc.email,               
+      subject: "File Uploaded Successfully",
+      html: `<p>Your file <b>${doc.name}</b> has been uploaded on Cloudinary.</p>`,
+    });
+
+    console.log("Mail sent:", info.messageId);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const File = mongoose.model("File",fileSchema);
